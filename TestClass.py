f993ca9cd55fa5159e0c49bba471edda928387a8
brivera93 <54905578+brivera93@users.noreply.github.com>
2022-04-30 17:42:42 -0700

    Add files via upload

---
+++ TestClass.py
@@ -0,0 +1,21 @@
+#Play with strings class
+class StringPlayer:
+
+    def __init__(self, name, num1, num2):
+        self.name = name
+        self.num1 = num1
+        self.num2 = num2
+
+    # def reverse(self):
+    #     reverse = name[len(name)::-1]
+
+    def sum(self):
+        return self.num1 + self.num2
+
+    def getString(self):
+        return self.playString
+
+    def getNum(self):
+        return self.someNum
+
+
---
+++ main.py
@@ -0,0 +1,96 @@
+from TestClass import StringPlayer
+
+
+def bubbleSort(myList=[]):
+    length = len(myList)
+    for i in range(length - 1):
+        for j in range(0, length - i - 1):
+            if myList[j] > myList[j + 1]:
+                myList[j], myList[j + 1] = myList[j + 1], myList[j]
+
+
+def addNumber(num1, num2):
+    # sum = 0
+    sum = num1 + num2
+    return sum
+
+
+test = StringPlayer("Bryan", 100, 250)
+print(test.sum())
+
+num1 = 300
+num2 = 200
+
+# if num1 > num2:
+#     print('num1 is greater')
+# elif num1 == num2:
+#     print('num1 is equal to num2')
+# else:
+#     print("num1 is less than")
+# list = 1, 3, 5, 7, 9
+#
+# for var in list:
+#     print(var)
+
+list = 20, 30, 10, 25, 5, 15, 70
+sum = 0
+
+while sum < 100:
+    sum += 23
+    if sum < 100:
+        print(sum)
+
+
+
+
+
+
+print('hello world')
+
+
+
+# lingo = "bingo"
+# dubNum = 42.10293
+# listy = [1, 3, 5, 7, 9]
+# listy2 = [4, 1, 3, 9, 8, 6, 2, 7, 5]
+# listy3 = [9, 3, 2, 5]
+#
+# for var in listy2:
+#     print("element " + str(var))
+#
+# for var in range(4,9):
+#     print(var)
+#
+# test2 = "we are going to reverse a really long string, easily"
+# test1 = StringPlayer("BryanRivers", 28)
+# print(test1.getString() + " " + str(test1.getNum()) + "how long does it go")
+# print("Will this be on a new line")
+# print(type(lingo))
+# print(f"my name is {lingo} before reassignment")
+# print(lingo[len(lingo)::-1])
+# lingo = "zingo"
+# print(f"my name is {lingo} after reassignment")
+# print(type(listy))
+# print(listy2)
+# print(type(listy2))
+# bubbleSort(listy2)
+# print(listy2)
+# print(lingo[len(lingo)::-1])
+# print(test2)
+# print(test2[len(test2)::-1])  # this worked perfectly
+#
+# # lets try making a dictionary
+# dict1 = ([('A', 1), ('B', 2), ('C', 3)])
+# print(dict1)
+#
+# number1 = 100
+# number2 = 250
+# print(addNumber(number1, number2))
+
+# number = 0
+# listy.insert(0, number)
+
+# for number in range(len(listy)):
+# print(listy[number])
+# print(number)
+# print("")
