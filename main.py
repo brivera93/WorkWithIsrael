5fb242da3b64f2c27c6c40a5ca036030ff256883
brivera93 <54905578+brivera93@users.noreply.github.com>
2022-06-07 20:52:47 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -15,126 +15,152 @@ def addNumber(num1, num2):
     return sum
 
 
-# test = Practice("Bryan", 100, 250)
-# print(test.sum())
+def factorial(num):
+    if num < 2:
+        return 1
+    return num * factorial(num-1)
+#
+# 3 * factorial(3-1)
+# 3 * factorial(2)
+# 3 * 2 * factorial(2-1)
+# 3 * 2 * factorial(1)
+# 3 * 2 * 1
 
-num1 = 300
-num2 = 200
 
-# if num1 > num2:
-#     print('num1 is greater')
-# elif num1 == num2:
-#     print('num1 is equal to num2')
+
+def romanToInt(romanNum):
+    sum = 0
+    index = 0
+    while index < len(romanNum):
+        # get value of current symbol
+        currentVal = value(romanNum[index])
+        if (index + 1) < len(romanNum):
+            nextVal = value(romanNum[index+1])
+            if currentVal >= nextVal:
+                sum = sum + currentVal
+                index = index + 1
+            else:
+                sum = sum + nextVal - currentVal
+                index = index + 2
+
+    return sum
+
+
+def value(val):
+    if val == 'I':
+        return 1
+    if val == 'V':
+        return 5
+    if val == 'X':
+        return 10
+    if val == 'L':
+        return 50
+    if val == 'C':
+        return 100
+    if val == 'D':
+        return 500
+    if val == 'M':
+        return 1000
+    return -1
+
+#
+# {1,2,3,4,5,6,8}
+# 1,2,3,4,5,6,8
+
+
+def missingNum(array = []):
+    # length = len(array)
+    missingValue = set(range(array[0], array[-1] + 1)) - set(array)
+    return missingValue
+
+
+def reverseString(str):
+    length = len(str)
+    if length <= 1:
+        return str
+    low = 0
+    high = length - 1
+    while low < high:
+        str[low], str[high] = str[high], str[low]
+        low += 1
+        high -= 1
+    return str
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        return self.stack.pop()
+
+
+class Palindrome:
+    def __init__(self, string, stack_object):
+        self.string = string
+        self.obj = stack_object
+
+    def check(self):
+        length = len(self.string)
+
+        for ch in self.string:
+            self.obj.push(ch)
+
+        rev_string = ""
+        for i in range(length):
+            rev_string += self.obj.pop()
+
+        if rev_string == self.string:
+            return True
+        else:
+            return False
+
+#
+# obj1 = Practice("Bryan", 42, 99)
+# print("tbis is name in reverse " + obj1.reverse())
+# print("this is the sum of " + str(obj1.num1) + " and " + str(obj1.num2) + " is " + str(obj1.sum()))
+#
+#
+# num1 = 300
+# num2 = 200
+
+# numArray = 20, 30, 10, 5, 15, 70
+
+
+print(romanToInt("MCMIV"))
+
+
+
+list1 = [1,2,3,5]
+list2 = [0,2,3,4,5]
+list3 = [0,1,2,3,4,5,6,7,9]
+
+print(missingNum(list1))
+print(missingNum(list2))
+print(missingNum(list3))
+random = ['h', 'e', 'l', 'l', 'o']
+# olleh
+# oellh
+
+print(random)
+print(reverseString(random))
+
+# string = input("Enter a string: ")
+# stack_obj = Stack()
+# obj = Palindrome(string, stack_obj)
+# if obj.check():
+#     print("The given string is a palindrome!")
 # else:
-#     print("num1 is less than")
-# list = 1, 3, 5, 7, 9
-#
-# for var in list:
-#     print(var)
-
-# example of tuple
-numArray = 20, 30, 10, 5, 15, 70
-
-# setArray = {1, 1, 2, 2, 3, 4, 5}
-# print(setArray)
+#     print("The given string is not a palindrome!")
 
 #
-# # example of list
-# listArray = [1, 23, 94, 23]
-# for var in listArray:
-#     print(var)
-#
-# listArray.insert(1, 1000)
-# # listArray.__add__(2000)
-# listArray.remove(94)
-# for var in listArray:
-#     print(var)
+# val = input("Enter input 0-6\n")
+# value = int(val)
+# print(factorial(value))
 
-# print(type(numArray))
-# print("25 exists in list") if 25 in numArray else print("25 dne in list")
-
-# while sum < 100:
-#     sum += 23
-#     if sum < 100:
-#         print(sum)
-#
-# sum = 0
-# i = 0
-# print("while looping using list")
-# while sum < 100:
-#     sum += numArray[i]
-#     i += 1
-#     if sum < 100:
-#         print(sum)
-
-# print(test.getName())
-# print(test.reverse())
-
-# dictionary examples
-
-# peopleAges = dict(Bryan=28, Ana=21, Felipe=99, Jasmyn=12)
-# print(peopleAges)
-# peopleAges.update(Bryan=101)
-# print(peopleAges)
-# peopleAges['Israel'] = 93
-# print(peopleAges)
-# peopleAges.pop('Felipe')
-# print(peopleAges)
-# peopleAges.popitem()
-# print(peopleAges)
-#
-# dictCopy = peopleAges.copy()
-# print(dictCopy)
-
-
-# lingo = "bingo"
-# dubNum = 42.10293
-# listy = [1, 3, 5, 7, 9]
-# listy2 = [4, 1, 3, 9, 8, 6, 2, 7, 5]
-# listy3 = [9, 3, 2, 5]
-#
-# for var in listy2:
-#     print("element " + str(var))
-#
-# for var in range(4,9):
-#     print(var)
-#
-# test2 = "we are going to reverse a really long string, easily"
-# test1 = StringPlayer("BryanRivers", 28)
-# print(test1.getString() + " " + str(test1.getNum()) + "how long does it go")
-# print("Will this be on a new line")
-# print(type(lingo))
-# print(f"my name is {lingo} before reassignment")
-# print(lingo[len(lingo)::-1])
-# lingo = "zingo"
-# print(f"my name is {lingo} after reassignment")
-# print(type(listy))
-# print(listy2)
-# print(type(listy2))
-# bubbleSort(listy2)
-# print(listy2)
-# print(lingo[len(lingo)::-1])
-# print(test2)
-# print(test2[len(test2)::-1])  # this worked perfectly
-#
-# # lets try making a dictionary
-# dict1 = ([('A', 1), ('B', 2), ('C', 3)])
-# print(dict1)
-#
-# number1 = 100
-# number2 = 250
-# print(addNumber(number1, number2))
-
-# number = 0
-# listy.insert(0, number)
-
-# for number in range(len(listy)):
-# print(listy[number])
-# print(number)
-# print("")
-
-val = input("Enter input 0-6")
-print(val)
 
 
 
