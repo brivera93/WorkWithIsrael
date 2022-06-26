ee38af6564f6ebee516283103c9ba25333e4e27c
brivera93 <54905578+brivera93@users.noreply.github.com>
2022-06-25 18:26:01 -0700

    Add files via upload

--- Algorithms.py
+++ Algorithms.py
@@ -1,4 +1,65 @@
 class Algorithms:
-    def __init__(self, name):
+
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
+
+    def printName(self):
+        print(self.name)
+
+    def factorial(self, num):
+        if num < 2:
+            return 1
+        else:
+            return num * self.factorial(num-1)
+
+    @staticmethod
+    def addTwoLists(list1, list2):
+        ptr1 = len(list1) - 1  # point to last element in list1
+        endList1 = False
+        ptr2 = len(list2) - 1  # point to last element in list2
+        endList2 = False
+        sumList = []  # list containing some of list1, list2
+        carry = False
+        # currentSum = 0
+        while not endList1 or not endList2:
+            if endList1 and not endList2:
+                if carry:
+                    currentSum = list2[ptr2] + 1
+                    if currentSum < 10:
+                        carry = False
+                        sumList.append(currentSum)
+                    else:
+                        sumList.append(0)
+                else:
+                    sumList.append(list2[ptr2])
+                ptr2 -= 1
+            elif not endList1 and endList2:
+                if carry:
+                    currentSum = list1[ptr1] + 1
+                    if currentSum < 10:
+                        carry = False
+                        sumList.append(currentSum)
+                    else:
+                        sumList.append(0)
+                else:
+                    sumList.append(list1[ptr1])
+                ptr1 -= 1
+            else:  # not at the end of list1 or list 2 (i.e. there items to be processed in both lists)
+                currentSum = list1[ptr1] + list2[ptr2] + 1 if carry else list1[ptr1] + list2[ptr2]
+                carry = True if currentSum >= 10 else False
+                if carry:
+                    numStr = str(currentSum)
+                    sumList.append(int(numStr[1]))
+                else:
+                    sumList.append(currentSum)
+                ptr1 -= 1
+                ptr2 -= 1
+            endList1 = True if ptr1 < 0 else False
+            endList2 = True if ptr2 < 0 else False
+        if carry:
+            sumList.append(1)
+        return sumList
+
+
 
--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+from Algorithms import Algorithms
+
+
+def strStr(needle, haystack):
+    ptrH = 0  # Used to parse through haystack string
+    startIndex = 0
+    ptrN = -1
+    strN = ""
+    if needle == " ":
+        return -1
+    while ptrH < len(haystack):
+        if haystack[ptrH] == needle[0] and startIndex == 0:
+            ptrN = ptrH
+            ptrH += 1
+            startIndex += 1
+            strN += needle[0]
+            # str += f'{needle[0]}'
+        elif haystack[ptrH] == needle[startIndex]:
+            ptrH += 1
+            strN += needle[startIndex]
+            # str += f'{needle[startIndex]}'
+            startIndex += 1
+            if needle == strN:
+                break
+        else:
+            ptrH += 1
+            ptrN = -1
+    return ptrN
+
+
 def isPalindrome(string):
     stack = []
     index = 0
@@ -21,6 +51,7 @@ def isPalindrome(string):
         print("the string " + string + " is a palindrome")
     else:
         print("the string " + string + " is not a palindrome")
+
 
 # dood
 def isPalindromePtr(string):
@@ -51,25 +82,56 @@ def mergeSortedLists(list1, list2):
         else:
             newList.append(list2[ptr2])
             ptr2 += 1
+    # accounts for last element to be added
     if ptr1 < ptr2:
         newList.append(list1[ptr1])
     else:
         newList.append(list2[ptr2])
     return newList
 
-
-str1 = "racecar"
-str2 = "bojangles"
-str3 = "bennynneb"
-isPalindrome(str1)
-isPalindrome(str2)
-isPalindrome(str3)
-l1 = [1, 3, 5, 7, 9]
-l2 = [2, 4, 6, 8, 10]
-sortedList = mergeSortedLists(l1, l2)
-print(sortedList)
+#
+# str1 = "racecar"
+# str2 = "bojangles"
+# str3 = "bennynneb"
+# isPalindrome(str1)
+# isPalindrome(str2)
+# isPalindrome(str3)
+# l1 = [1, 3, 5, 7, 9]
+# l2 = [2, 4, 6, 8, 10]
+# l3 = [11, 33, 55, 77, 99]
+# l4 = [22, 32, 33, 58, 1010]
+# l5 = [13, 69, 127, 349, 1001]
+# l6 = [11, 34, 128, 312, 900]
+# sortedList1 = mergeSortedLists(l1, l2)
+# print(sortedList1)
+# sortedList2 = mergeSortedLists(l3, l4)
+# print(sortedList2)
+# sortedList3 = mergeSortedLists(l5, l6)
+# print(sortedList3)
 # print("this is a break")
 # isPalindromePtr(str1)
 # isPalindromePtr(str2)
 # isPalindromePtr(str3)
+
+
+# needles = "hell", "nia", "kep", "nav", "llowe"
+# haystacks = "hello", "california", "keeper", "banana", "halloween"
+# for index in range(5):
+#     print("Index of needle in haystack " + str(index)
+#           + " is = " + str(strStr(needles[index], haystacks[index])))
+
+list1 = [1, 3, 5, 7, 9]
+list2 = [2, 4, 6, 8, 9]
+list3 = [9, 9, 9, 9, 9, 9, 9]
+list4 = [9, 9, 9, 9]
+list5 = [9, 8, 4, 7]
+list6 = [6, 8, 1, 2, 9, 5, 4]
+newList1 = Algorithms.addTwoLists(list1, list2)
+newList2 = Algorithms.addTwoLists(list3, list4)
+newList3 = Algorithms.addTwoLists(list5, list6)
+
+print(newList1)
+print(newList2)
+print(newList3)
+# print(list1[-1])
 
